69eb5381973b8c77a56bb0788943e947a874df26
VictoriaV <victoria.vandeweyer@outlook.be>
2021-02-21 03:16:34 +0100

    feat(model Board add moveColumn)

--- anc3_2021_projet_A06/src/main/java/model/Board.java
+++ anc3_2021_projet_A06/src/main/java/model/Board.java
@@ -39,4 +39,27 @@ public class Board {
     public String toString() {
         return title;
     }
+
+    public void moveColumn(Column column, Direction direction) {
+        if (direction == Direction.DOWN || direction == Direction.UP)
+            throw new UnsupportedOperationException();
+
+        if (direction == Direction.RIGHT) {
+            Optional<Column> nextColumn = getColumns().stream().sorted().filter(c -> c.getPosition().compareTo(column.getPosition()) > 0).findFirst();
+            if (nextColumn.isPresent()) {
+                Integer temp = nextColumn.get().getPosition();
+                nextColumn.get().setPosition(column.getPosition());
+                column.setPosition(temp);
+            }
+        } else if (direction == Direction.LEFT) {
+            Collections.reverse(getColumns());
+            Optional<Column> previousColumn = getColumns().stream().filter(c -> c.getPosition().compareTo(column.getPosition()) < 0).findFirst();
+            if (previousColumn.isPresent()) {
+                Integer temp = previousColumn.get().getPosition();
+                previousColumn.get().setPosition(column.getPosition());
+                column.setPosition(temp);
+            }
+        }
+
+    }
 }
