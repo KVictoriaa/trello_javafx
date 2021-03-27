707c2bb2562a6e586c9072971a56e6ed0d4b9338
VictoriaV <victoria.vandeweyer@outlook.be>
2021-03-28 00:14:26 +0100

    feat(View Trelloview):implement trelloview

---
+++ anc3_2021_projet_A06/src/main/java/view/TrelloView.java
@@ -0,0 +1,48 @@
+package view;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import mvvm.ViewModelBoard;
+
+public class TrelloView extends VBox {
+    private ViewModelBoard viewModelBoard;
+    private ViewBoard viewBoard;
+    private Stage stage;
+    private HBox down = new HBox();
+    private Scene scene;
+
+
+    public TrelloView(ViewModelBoard viewModelBoard, Stage stage) {
+        this.viewBoard = new ViewBoard(viewModelBoard);
+        this.viewModelBoard = viewModelBoard;
+        this.stage = stage;
+        scene = new Scene(this, 900,500);
+        stage.setTitle("Trello");
+        stage.setScene(scene);
+        configTrello();
+
+
+    }
+    private void configTrello(){
+
+
+        down.getChildren().addAll(viewBoard);
+        this.getChildren().addAll(down);
+
+
+    }
+
+}
