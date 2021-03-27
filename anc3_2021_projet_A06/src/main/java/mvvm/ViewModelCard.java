99bfdf09df3c8d0328dd64484b0db74c6344da03
Nordinne Ben Haddou <nordinnebenhaddou@hotmail.com>
2021-03-28 00:44:33 +0100

    feat(ViewModelCard): implement Move

--- anc3_2021_projet_A06/src/main/java/mvvm/ViewModelCard.java
+++ anc3_2021_projet_A06/src/main/java/mvvm/ViewModelCard.java
@@ -1,6 +1,10 @@
 package mvvm;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import model.Card;
+
+import java.util.Optional;
 
 
 public class ViewModelCard {
@@ -19,5 +23,35 @@ public class ViewModelCard {
         return card;
     }
 
+    public void moveUp() {
+
+        card.getColumn().moveCardUp(card);
+    }
+
+    public void moveDown() {
+
+        card.getColumn().moveCardDown(card);
+    }
+
+    public void moveLeft() {
+        card.getColumn().moveCardLeft(card);
+    }
+
+    public void moveRight() {
+        card.getColumn().moveCardRight(card);
+    }
+
+    public void removeCard() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Suppression");
+        alert.setContentText("Voulez-vous vraiment supprimer cette carte?");
+        Optional<ButtonType> res = alert.showAndWait();
+        if (res.isPresent() && res.get() == ButtonType.OK) {
+            card.getColumn().removeCardList(card);
+        }
+    }
+    public void setTitle(String title) {
+    }
 
 }
+
