97a4302512b566107633f562fa47d07031d13ef1
Nordinne Ben Haddou <nordinnebenhaddou@hotmail.com>
2021-03-28 00:18:00 +0100

    feat(Main) : Add Board , ViewModelBoar

--- anc3_2021_projet_A06/src/main/java/main/Main.java
+++ anc3_2021_projet_A06/src/main/java/main/Main.java
@@ -2,13 +2,18 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.Board;
+import mvvm.ViewModelBoard;
+import view.TrelloView;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Board board = new Board("Tableau");
+        ViewModelBoard viewModelBoard = new ViewModelBoard(board);
+        new TrelloView(viewModelBoard,primaryStage);
         primaryStage.show();
     }
 
