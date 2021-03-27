40155502730638359704d31587647be2f452622d
VictoriaV <victoria.vandeweyer@outlook.be>
2021-03-27 22:47:32 +0100

    refractor(card.model,board.model)

--- anc3_2021_projet_A06/src/main/java/main/Main.java
+++ anc3_2021_projet_A06/src/main/java/main/Main.java
@@ -2,18 +2,12 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import model.Board;
-import mvvm.ViewModel;
-import view.View;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Board board = new Board();
-        ViewModel viewModel = new ViewModel(board);
-        View view = new View(primaryStage, viewModel);
 
         primaryStage.show();
     }
--- anc3_2021_projet_A06/src/main/java/model/Card.java
+++ anc3_2021_projet_A06/src/main/java/model/Card.java
@@ -1,2 +1,50 @@
-package model;public class Card {
+package model;
+
+public class Card {
+    private String name;
+    private int position;
+    Column column;
+
+    public Card(String name, int position,Column column) {
+        this.name = name;
+        this.position = position;
+        this.column = column;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+    public Column getColumn() {
+        return column;
+    }
+
+    public void setColumn(Column column) {
+        this.column = column;
+    }
+
+    public boolean isFirtPosition(){
+        return getPosition() == 1;
+    }
+    public boolean isLastPostion(){
+        return column.getLastPosition() == getPosition();
+    }
+
+
+    @Override
+    public String toString() {
+        return "" + position;
+    }
 }
