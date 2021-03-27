71c39230a9c99e86b7bb0de9701ea3e0af6495ca
Nordinne Ben Haddou <nordinnebenhaddou@hotmail.com>
2021-03-28 00:55:54 +0100

    feat(ViewColumn): -bindind -customize() -configAction()

--- anc3_2021_projet_A06/src/main/java/view/ViewColumn.java
+++ anc3_2021_projet_A06/src/main/java/view/ViewColumn.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.Card;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ViewColumn extends VBox {
-
+    private EditableLabel editableLabel;
     private ViewModelColumn viewModelColumn;
     private ListView<Card> cardListView = new ListView<>();
     private List<ViewCard> cards = new ArrayList<>();
@@ -30,14 +31,16 @@ public class ViewColumn extends VBox {
     public ViewColumn(Column column, ViewModelBoard viewModelBoard) {
         this.viewModelBoard = viewModelBoard;
         viewModelColumn = new ViewModelColumn(column, viewModelBoard);
+        editableLabel = new EditableLabel(viewModelColumn.columnNameProperty(), null, viewModelColumn, null);
         configColumn();
         configData();
         customize();
+        configAction();
 
     }
 
     private void configColumn() {
-
+        up.getChildren().addAll(left, editableLabel, right);
         down.getChildren().addAll(cardListView);
         this.getChildren().addAll(up, down);
         up.setPrefWidth(250);
@@ -47,6 +50,8 @@ public class ViewColumn extends VBox {
 
     private void configData() {
         cardListView.itemsProperty().bind(viewModelColumn.cardsProperty());
+        left.disableProperty().bind(viewModelColumn.disableColumnLeftProperty());
+        right.disableProperty().bind(viewModelColumn.disableColumnRightProperty());
 
     }
 
@@ -63,5 +68,25 @@ public class ViewColumn extends VBox {
             }
         });
     }
+    public void configAction(){
 
+        left.setOnMouseClicked(e -> {
+            viewModelColumn.moveLeft();
+        });
+        right.setOnMouseClicked(e ->{
+            viewModelColumn.moveRight();
+        });
+        cardListView.setOnMouseClicked(e -> {
+            if(e.getClickCount() == 3){
+                viewModelColumn.addCard();
+            }
+        });
+        this.setOnMouseClicked(e -> {
+            if(e.getButton() == MouseButton.SECONDARY){
+                viewModelColumn.removeColumn();
+            }
+        });
+
+    }
 }
+
