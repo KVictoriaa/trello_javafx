9425c7f88039ab34e1e7c0922c4fa77be2dec68c
Nordinne Ben Haddou <nordinnebenhaddou@hotmail.com>
2021-03-28 01:30:59 +0100

    feat(ColumnCommand) : implement AddCardCommand

---
+++ anc3_2021_projet_A06/src/main/java/model/column/AddCardCommand.java
@@ -0,0 +1,30 @@
+package model.column;
+
+import model.Card;
+import model.Column;
+
+public class AddCardCommand extends ColumnCommand {
+    private final Card card;
+    public AddCardCommand(Column column, Card card) {
+        super(column);
+        this.card = card;
+    }
+
+    @Override
+    public void execute() {
+        this.getColumn().addCardList(card);
+    }
+
+    @Override
+    public void undo() {
+        this.getColumn().removeCardList(card);
+    }
+
+    @Override
+    public boolean canBeUndone() {
+        return true;
+    }
+    public String getNameAction(){
+        return "ajout d'une carte";
+    }
+}
