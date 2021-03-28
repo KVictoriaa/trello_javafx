9dab877b24aa482bf64733e7e81fe9a88be00aba
VictoriaV <victoria.vandeweyer@outlook.be>
2021-03-28 03:07:00 +0200

    feat(iteration2: model board):add methode undo redo and class moveColumnToLeft

---
+++ anc3_2021_projet_A06/src/main/java/model/board/MoveColumnToLeft.java
@@ -0,0 +1,30 @@
+package model.board;
+
+import model.Column;
+import model.column.ColumnCommand;
+
+public class MoveColumnToLeft extends ColumnCommand {
+    public MoveColumnToLeft(Column column) {
+        super(column);
+    }
+
+    @Override
+    public void execute() {
+        this.getColumn().getBoard().moveColumnLeft(getColumn());
+    }
+
+    @Override
+    public void undo() {
+        this.getColumn().getBoard().moveColumnRight(getColumn());
+    }
+
+    @Override
+    public boolean canBeUndone() {
+        return true;
+    }
+
+    @Override
+    public String getNameAction() {
+        return " mouvement de la colonne vers la gauche";
+    }
+}
