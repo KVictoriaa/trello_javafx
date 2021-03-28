a5497cd2a225c36c72a8678c318330d99a20c315
VictoriaV <victoria.vandeweyer@outlook.be>
2021-03-28 03:08:39 +0200

    feat(iteration2: model board):add methode undo redo and class removeColumnCommand

---
+++ anc3_2021_projet_A06/src/main/java/model/board/RemoveColumnCommand.java
@@ -0,0 +1,30 @@
+package model.board;
+
+import model.Column;
+import model.column.ColumnCommand;
+
+public class RemoveColumnCommand extends ColumnCommand {
+    public RemoveColumnCommand(Column column) {
+        super(column);
+    }
+
+    @Override
+    public void execute() {
+        this.getColumn().getBoard().removeColumns(getColumn());
+    }
+
+    @Override
+    public void undo() {
+        this.getColumn().getBoard().addColumns(getColumn());
+    }
+
+    @Override
+    public boolean canBeUndone() {
+        return true;
+    }
+
+    @Override
+    public String getNameAction() {
+        return "suppression de colonne";
+    }
+}
