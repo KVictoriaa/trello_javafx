2003782331171de4063a531806ebc05daae9351a
Nordinne Ben Haddou <nordinnebenhaddou@hotmail.com>
2021-03-28 02:20:18 +0100

    feat(ViewModelColumn): implement moves and remove

--- anc3_2021_projet_A06/src/main/java/mvvm/ViewModelColumn.java
+++ anc3_2021_projet_A06/src/main/java/mvvm/ViewModelColumn.java
@@ -8,6 +8,12 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import model.Card;
 import model.Column;
+import model.Processor;
+import model.board.MoveColumnToLeft;
+import model.board.MoveColumnToRight;
+import model.board.RemoveColumnCommand;
+import model.column.AddCardCommand;
+import model.column.SetTitleColumnCommand;
 
 import java.util.Optional;
 
@@ -42,17 +48,31 @@ public class ViewModelColumn {
         return cards;
     }
 
-    public void moveLeft() {
-        column.getBoard().moveColumnLeft(column);
-    }
-
-    public void moveRight() {
-        column.getBoard().moveColumnRight(column);
-    }
 
     public void addCard() {
         Card card = new Card("card" + (column.getCardList().size() + 1), column.getCardList().size() + 1, column);
         column.addCardList(card);
+        AddCardCommand addCardCommand = new AddCardCommand(column,card);
+        Processor.getInstance().execute(addCardCommand);
+        viewModelBoard.refreshUndoRedoProperty();
+
+    }
+    public void moveLeft(){
+
+
+        MoveColumnToLeft moveColumnToLeft = new MoveColumnToLeft(column);
+        Processor.getInstance().execute(moveColumnToLeft);
+        viewModelBoard.refreshUndoRedoProperty();
+
+    }
+    public void moveRight(){
+
+
+        MoveColumnToRight moveColumnToRight = new MoveColumnToRight(column);
+        Processor.getInstance().execute(moveColumnToRight);
+        viewModelBoard.refreshUndoRedoProperty();
+
+
     }
     public void removeColumn() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -61,10 +81,17 @@ public class ViewModelColumn {
         Optional<ButtonType> res = alert.showAndWait();
         if (res.isPresent() && res.get() == ButtonType.OK) {
             column.getBoard().removeColumns(column);
+            RemoveColumnCommand removeColumnCommand = new RemoveColumnCommand(column);
+            Processor.getInstance().execute(removeColumnCommand);
         }
+        viewModelBoard.refreshUndoRedoProperty();
+
     }
-    public void setTitle(String title) {column.setName(title);
+    public void setTitle(String title){
+        Processor.getInstance().execute(new SetTitleColumnCommand(column ,title ,column.getName()));
+        viewModelBoard.refreshUndoRedoProperty();
     }
+
         public SimpleBooleanProperty disableColumnLeftProperty() {
         return disableColumnLeft;
     }
