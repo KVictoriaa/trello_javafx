88263123ad771da19261478c1c81d1b77bed94c9
VictoriaV <victoria.vandeweyer@outlook.be>
2021-05-12 17:34:06 +0200

    feat(RemoveColumnCommand):move to package mvvm

--- anc3_2021_projet_A06/src/main/java/mvvm/board/RemoveColumnCommand.java
+++ anc3_2021_projet_A06/src/main/java/mvvm/board/RemoveColumnCommand.java
@@ -1,7 +1,7 @@
 package mvvm.board;
 
 import model.Column;
-import model.column.ColumnCommand;
+import mvvm.column.ColumnCommand;
 
 public class RemoveColumnCommand extends ColumnCommand {
     public RemoveColumnCommand(Column column) {
