a2cb45819a29e4c1af271f16221eaadb759737e0
VictoriaV <victoria.vandeweyer@outlook.be>
2021-05-12 21:04:15 +0200

    feat(model/Board):add refreshdata()

--- anc3_2021_projet_A06/src/main/java/model/Board.java
+++ anc3_2021_projet_A06/src/main/java/model/Board.java
@@ -143,4 +143,28 @@ public class Board {
             throwables.printStackTrace ();
         }
     }
+    public void initData(){
+        Column column1 = new Column(1,"column1",1,this);
+        Column column2 = new Column(2,"column2",2,this);
+        Column column3 = new Column(3,"column3",3,this);
+
+        Card card1 = new Card(1,"card1", 1,column1);
+        Card card2 = new Card(2,"card2", 1,column2);
+        Card card3 = new Card(3,"card3", 2,column2);
+        Card card4 = new Card(4,"card4", 1,column3);
+        Card card5 = new Card(5,"card5", 2,column3);
+        Card card6 = new Card(6,"card6", 3,column3);
+
+        addColumns(column1);
+        addColumns(column2);
+        addColumns(column3);
+
+        column1.addCardListDao(card1);
+        column2.addCardListDao(card2);
+        column2.addCardListDao(card3);
+        column3.addCardListDao(card4);
+        column3.addCardListDao(card5);
+        column3.addCardListDao(card6);
+    }
+
 }
